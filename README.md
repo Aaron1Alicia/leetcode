c9036e776ed6903378a65df9fd69f3f08fb5c289
Aaron1Alicia <aaron.wong@nyu.edu>
2014-01-17 01:09:17 -0500

    first ac

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 leetcode
 ========
+This folder contains the leetcode source code writeen by Aaron
---
+++ Single_Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+		    int result=0;
+				while(n!=0)
+				{
+					result^=A[n-1];
+					n--;
+				}
+
+				return result;
+        
+    }
+};
+
